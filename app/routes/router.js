dff5d9ccd4d9e9df2ce31743268176f1e3884e64
Gebê <rm93907@estudante.fieb.edu.br>
2025-08-03 16:08:43 -0300

    grupo de merda

--- app/routes/router.js
+++ app/routes/router.js
@@ -5,4 +5,8 @@ router.get('/', function(req,res ){
     res.render('pages/index');
 });
 
+router.get('/ilhagalapagos', function(req,res ){
+    res.render('pages/ilhagalapagos');
+});
+
 module.exports = router;
