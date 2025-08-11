a5915f91e9fbe9164e22a9efd3d193407dcb102f
Vitor M <rm94086@estudante.fieb.edu.br>
2025-08-11 19:54:11 -0300

    T.T

---
+++ app/public/js/scriptlogin.js
@@ -0,0 +1,9 @@
+    function mostrarCadastro() {
+      document.getElementById('login-form').classList.add('hidden');
+      document.getElementById('cadastro-form').classList.remove('hidden');
+    }
+
+    function mostrarLogin() {
+      document.getElementById('cadastro-form').classList.add('hidden');
+      document.getElementById('login-form').classList.remove('hidden');
+    }
